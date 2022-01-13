90f7463ac64441c10efd046c567a2b7165eaea5a
BariBaso <kissmarci41@gmail.com>
2022-01-13 09:52:07 +0100

    4. feladatig

---
+++ Fifa/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fifa
+{
+    class csapat
+    {
+        public string Csapat { get; set; }
+        public int Helyezes { get; set; }
+        public int Valtozas{ get; set; }
+        public int Pontszam { get; set; }
+
+        public csapat (string sor)
+        {
+            string[] t = sor.Split(';');
+            Csapat = t[0];
+            Helyezes = int.Parse(t[1]);
+            Valtozas = int.Parse(t[2]);
+            Pontszam = int.Parse(t[3]);
+
+        }
+        
+    }
+}
---
+++ Fifa/FIFAvilagranglista.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Fifa
+{
+    class FIFAvilagranglista
+    {
+        static void Main(string[] args)
+        {
+            List<csapat> csapatok = new List<csapat>();
+            foreach (var sor in File.ReadAllLines("fifa.txt").Skip(1))
+            {
+                csapatok.Add(new csapat(sor));
+            }
+
+
+            //3.
+            Console.WriteLine($"3. feladat: A világtanglistán {csapatok.Count} csapat szerepel");
+            //4.
+            Console.WriteLine($"4. feladat: A csapatok átlagos pontszáma: {csapatok.Average(x => x.Pontszam):0.00} pont");
+
+        }
+    }
+}
